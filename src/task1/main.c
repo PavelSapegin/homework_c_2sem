589c88a44c57e6362d7b72dd5be9d0119fdad0b4
PavelSapegin <pawsapegin@yandex.ru>
2026-03-04 22:10:44 +0300

    Add files

---
+++ src/task1/main.c
@@ -0,0 +1,12 @@
+#include "readcsv.h"
+
+int main(int argc, char* argv[])
+{
+    if ((argc > 1) && (strcmp(argv[1], "--test") == 0)) {
+        runTests();
+        return 0;
+    }
+
+    readCsv("../input.csv", "../output.txt");
+    return 0;
+}
