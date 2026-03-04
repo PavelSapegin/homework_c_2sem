035329780490f85c582c61c07e83bbf13e489cf3
PavelSapegin <pawsapegin@yandex.ru>
2026-03-04 21:07:58 +0300

    Formatting

--- src/task1/readcsv.c
+++ src/task1/readcsv.c
@@ -6,59 +6,62 @@
 #define MAX_BUFF 4096
 #define BIAS 2
 
-int isNum(char s[]) {
-  if ((s == NULL) || (*s == '\0'))
-    return 0;
+int isNum(char s[])
+{
+    if ((s == NULL) || (*s == '\0'))
+        return 0;
 
-  char *p;
-  strtod(s, &p);
-  return *p == '\0';
+    char* p;
+    strtod(s, &p);
+    return *p == '\0';
 }
 
-void printLine(FILE *fpwr, const size_t widths[], int colsCount, Len type) {
-  char *chars[3][4] = {
-      {"╔", "═", "╦", "╗"}, // Header
-      {"╠", "═", "╬", "╣"}, // Body
-      {"╚", "═", "╩", "╝"}  // Footer
-  };
+void printLine(FILE* fpwr, const size_t widths[], int colsCount, Len type)
+{
+    char* chars[3][4] = {
+        { "╔", "═", "╦", "╗" }, // Header
+        { "╠", "═", "╬", "╣" }, // Body
+        { "╚", "═", "╩", "╝" } // Footer
+    };
 
-  fputs(chars[type][0], fpwr);
-  for (int colIdx = 0; colIdx < colsCount; ++colIdx) {
-    for (size_t i = 0; i < widths[colIdx]; ++i) {
-      fputs(chars[type][1], fpwr);
-    }
+    fputs(chars[type][0], fpwr);
+    for (int colIdx = 0; colIdx < colsCount; ++colIdx) {
+        for (size_t i = 0; i < widths[colIdx]; ++i) {
+            fputs(chars[type][1], fpwr);
+        }
 
-    if (colIdx < colsCount - 1) {
-      fputs(chars[type][2], fpwr);
+        if (colIdx < colsCount - 1) {
+            fputs(chars[type][2], fpwr);
+        }
     }
-  }
-  fprintf(fpwr, "%s\n", chars[type][3]);
+    fprintf(fpwr, "%s\n", chars[type][3]);
 }
 
-void maxWidth(FILE *fp, size_t widths[], int *colsCount) {
-  char buff[MAX_BUFF];
-  long stPos = ftell(fp);
+void maxWidth(FILE* fp, size_t widths[], int* colsCount)
+{
+    char buff[MAX_BUFF];
+    long stPos = ftell(fp);
 
     while (fgets(buff, sizeof(buff), fp)) {
         int colIdx = 0;
         buff[strcspn(buff, "\r\n")] = 0;
         size_t newLen;
 
-    char *start = buff;
-    char *end = strchr(start, ',');
+        char* start = buff;
+        char* end = strchr(start, ',');
 
-    while ((end != NULL) && (colIdx < MAX_COLS)) {
-      *end = '\0';
+        while ((end != NULL) && (colIdx < MAX_COLS)) {
+            *end = '\0';
 
-      newLen = strlen(start);
+            newLen = strlen(start);
 
-      if (newLen > widths[colIdx])
-        widths[colIdx] = newLen + BIAS;
+            if (newLen > widths[colIdx])
+                widths[colIdx] = newLen + BIAS;
 
-      colIdx++;
-      start = end + 1;
-      end = strchr(start, ',');
-    }
+            colIdx++;
+            start = end + 1;
+            end = strchr(start, ',');
+        }
 
         // Последний столбец обрабатываем отдельно
         if (colIdx < MAX_COLS) {
@@ -68,20 +71,20 @@ void maxWidth(FILE *fp, size_t widths[], int *colsCount) {
             colIdx++;
         }
 
-    if (colIdx > *colsCount) {
-      *colsCount = colIdx;
+        if (colIdx > *colsCount) {
+            *colsCount = colIdx;
+        }
     }
-  }
 
-  fseek(fp, stPos, SEEK_SET);
+    fseek(fp, stPos, SEEK_SET);
 }
 
-void printRow(FILE *fpwr, char line[], int colsCount, size_t widths[],
-              int isHeader) {
-  line[strcspn(line, "\r\n")] = 0;
+void printRow(FILE* fpwr, char line[], int colsCount, size_t widths[], int isHeader)
+{
+    line[strcspn(line, "\r\n")] = 0;
 
-  char *start = line;
-  char *end = strchr(start, ',');
+    char* start = line;
+    char* end = strchr(start, ',');
 
     fprintf(fpwr, "║");
     for (int colIdx = 0; colIdx < colsCount; ++colIdx) {
@@ -102,28 +105,13 @@ void printRow(FILE *fpwr, char line[], int colsCount, size_t widths[],
         } else
             start = NULL;
     }
-
-    if (start == NULL) {
-      fprintf(fpwr, "%-*s", (int)widths[colIdx], " ");
-    } else if ((isNum(start)) && !(isHeader)) {
-      fprintf(fpwr, "%*s", (int)widths[colIdx], start);
-    } else {
-      fprintf(fpwr, "%-*s", (int)widths[colIdx], start);
-    }
-    fprintf(fpwr, "║");
-
-    if (end != NULL) {
-      start = end + 1;
-      end = strchr(start, ',');
-    } else
-      start = NULL;
-  }
-  fprintf(fpwr, "\n");
+    fprintf(fpwr, "\n");
 }
 
-int readCsv(char fileread[], char filewrite[]) {
-  FILE *fp = fopen(fileread, "r");
-  FILE *fpwr = fopen(filewrite, "w");
+int readCsv(char fileread[], char filewrite[])
+{
+    FILE* fp = fopen(fileread, "r");
+    FILE* fpwr = fopen(filewrite, "w");
 
     if ((fp == NULL) || (fpwr == NULL)) {
         printf("Error of opening reading or writing file\n");
@@ -140,9 +128,9 @@ int readCsv(char fileread[], char filewrite[]) {
     size_t widths[MAX_COLS] = { 0 };
     char buff[MAX_BUFF];
 
-  maxWidth(fp, widths, &colsCount);
+    maxWidth(fp, widths, &colsCount);
 
-  printLine(fpwr, widths, colsCount, Header);
+    printLine(fpwr, widths, colsCount, Header);
 
     while (fgets(buff, sizeof(buff), fp)) {
         if (!isFirstRow)
@@ -153,8 +141,8 @@ int readCsv(char fileread[], char filewrite[]) {
     }
     printLine(fpwr, widths, colsCount, Footer);
 
-  fclose(fp);
-  fclose(fpwr);
+    fclose(fp);
+    fclose(fpwr);
 
-  return 0;
+    return 0;
 }
