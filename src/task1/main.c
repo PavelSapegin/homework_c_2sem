a0353e1d3d3c55d1bf2b0a76b5a17773804becf8
PavelSapegin <pawsapegin@yandex.ru>
2026-03-04 21:03:03 +0300

    Formatting code

--- src/task1/main.c
+++ src/task1/main.c
@@ -1,11 +1,10 @@
 #include "readcsv.h"
 
-int main(int argc, char* argv[])
-{
-    if ((argc > 1) && (strcmp(argv[1], "--test") == 0)) {
-        runTests();
-        return 0;
-    }
-    readCsv("../input.csv", "../output.txt");
+int main(int argc, char *argv[]) {
+  if ((argc > 1) && (strcmp(argv[1], "--test") == 0)) {
+    runTests();
     return 0;
+  }
+  readCsv("../input.csv", "../output.txt");
+  return 0;
 }
